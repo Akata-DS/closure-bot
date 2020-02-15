c7b215221fb6f45f0784ffba2372a4a558bf671a
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-16 03:28:16 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
                         
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setActivity('Arknights');
+    client.user.setGame('Arknights');
 });
 
 client.on("message", async message => {
