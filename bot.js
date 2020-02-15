af65f1e768dc02b10b20cd70e1210a96e07012c7
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-16 02:46:50 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,31 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+});
+    
 client.on('guildMemberAdd', member => {
   console.log('User' + member.user.tag + 'has joined the server!');
 
   var role = member.guild.roles.find('name', 'user');
-  member.addRole('677515320453365788');
+  member.addRole('677515320453365788'); // Customer Role
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
