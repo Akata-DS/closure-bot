7b61c651a7eb82812e047ab6d743965186766e51
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-16 01:25:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,15 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find('name, 'Customer');
+    member.addRole(Customer)
+});
+
 client.on("message", async message => {
   if(message.author.bot) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 }
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
