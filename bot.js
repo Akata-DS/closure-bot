234171aa6ae678bc7cfe94d198cc99b05d6e9172
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-16 17:45:17 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ client.on('ready', () => {
 client.on("message", async message => {
   if(message.author.bot) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  if(message.content.indexOf(config.prefix) !== 0) return;
   const command = args.shift().toLowerCase();
   // COMMAND SECTION ===========================================
   if(command === "+say", message.member.roles.has('677351882292854825')) {
