cc5aedaf331edce68e592ca2f100e3f0c2035a13
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-16 17:38:36 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,13 +17,46 @@ client.on("message", async message => {
     const sayMessage = args.join(" ");
     message.channel.send(sayMessage);
   }
-  if(command === "color") {
-    message.channel.send('This shit is still being done, pls be patient');
-    const sayMessage = args.join(" ");
-    if(sayMessage === "White") {
-      message.channel.send(sayMessage);
-    } else {message.channel.send('That color is not white, no');}
+  // START OF EXPERIMENTAL AREA
+var emojiname = ["pastelpink","pastelyellow","pastelgreen","hotpink","purple","orange","peach","pastelpurple","lightblue","red"];
+var rolename=["Pastel Pink","Pastel Yellow","Pastel Green","Hot Pink","Purple","Orange","Peach","Pastel Purple","Light Blue","Red"];
+client.on('message', msg => {
+
+  if(msg.content.startsWith("reaction")){
+    if(!msg.channel.guild) return;
+    for(let n in emojiname){
+    var emoji =[msg.guild.emojis.find(r => r.name == emojiname[n])];
+    for(let i in emoji){
+      msg.react(emoji[i]);
+    }
+   }
   }
+  });
+
+client.on("messageReactionAdd",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);          
+    reaction.message.guild.member(user).addRole(role).catch(console.error);
+  }
+}
+});
+
+client.on("messageReactionRemove",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);   
+    reaction.message.guild.member(user).removeRole(role).catch(console.error);
+  }
+  }
+});
+  // END OF THE EXPERIMENTAL AREA
 });
 
 client.on('guildMemberAdd', member => {
