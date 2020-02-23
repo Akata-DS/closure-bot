bdad4dfa6c33158e877d5ec2cadc3345038c0266
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-23 12:07:34 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const client = new Discord.Client();
 const config = require("./config.json");
                         
 client.on('ready', () => {
     console.log('COMPLETED');
     client.user.setActivity('Arknights');
 });
-
 client.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
@@ -40,7 +39,7 @@ client.on('message', msg => {
    }
   }
   });
-
+channel.fetchMessage('99539446449315840');
 client.on("messageReactionAdd",(reaction,user)=>{
   if(!user) return;
   if(user.bot)return;
