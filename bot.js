e4a901c0460b8008bab31e8e9157ec1fbee6eb90
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-23 11:05:37 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,9 +21,9 @@ client.on("message", async message => {
     const pictureLink = args.join(" ");
     message.channel.send({files: [pictureLink]});
   }
-  const exampleEmbed = require("./silverashS3.json");
+  const silverashS3 = require("./silverashS3.json");
   if(command === "embedtest") {
-    message.channel.send(exampleEmbed);
+    message.channel.send(silverashS3);
   }
   // START OF EXPERIMENTAL AREA
 var emojiname = ["PastelPink","PastelYellow","PastelGreen","HotPink","Purple","Orange","Peach","PastelPurple","LightBlue","Red"];
