c29d42a2ec3c1f3f4992f69184989630a1eaa58a
Akata-DS <61079492+Akata-DS@users.noreply.github.com>
2020-02-23 12:15:22 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ client.on('ready', () => {
     console.log('COMPLETED');
     client.user.setActivity('Arknights');
 });
+
 client.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
@@ -24,46 +25,6 @@ client.on("message", async message => {
   if(command === "embedtest") {
     message.channel.send({embed: aaa});
   }
-  // START OF EXPERIMENTAL AREA
-var emojiname = ["PastelPink","PastelYellow","PastelGreen","HotPink","Purple","Orange","Peach","PastelPurple","LightBlue","Red"];
-var rolename=["Pastel Pink","Pastel Yellow","Pastel Green","Hot Pink","Purple","Orange","Peach","Pastel Purple","Light Blue","Red"];
-client.on('message', msg => {
-
-  if(msg.content.startsWith("reaction")){
-    if(!msg.channel.guild) return;
-    for(let n in emojiname){
-    var emoji =[msg.guild.emojis.find(r => r.name == emojiname[n])];
-    for(let i in emoji){
-      msg.react(emoji[i]);
-    }
-   }
-  }
-  });
-channel.fetchMessage('680761697958494208');
-client.on("messageReactionAdd",(reaction,user)=>{
-  if(!user) return;
-  if(user.bot)return;
-  if(!reaction.message.channel.guild) return;
-  for(let n in emojiname){
-  if(reaction.emoji.name == emojiname[n]){
-    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);          
-    reaction.message.guild.member(user).addRole(role).catch(console.error);
-  }
-}
-});
-
-client.on("messageReactionRemove",(reaction,user)=>{
-  if(!user) return;
-  if(user.bot)return;
-  if(!reaction.message.channel.guild) return;
-  for(let n in emojiname){
-  if(reaction.emoji.name == emojiname[n]){
-    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);   
-    reaction.message.guild.member(user).removeRole(role).catch(console.error);
-  }
-  }
-});
-  // END OF THE EXPERIMENTAL AREA
 });
 
 client.on('guildMemberAdd', member => {
